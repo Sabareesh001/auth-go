9ab323e88b5b7177b1359eb9e40e6bfb94e8df46
Sabareesh001 <hatrickda@gmail.com>
2025-05-30 16:23:11 +0530

    completed user_pass validation method

--- authentication/user_pass/user_pass.go
+++ authentication/user_pass/user_pass.go
@@ -1,1 +1,20 @@
 package user_pass
+
+import (
+	"gorm.io/gorm"
+)
+
+func ValidateUserPass(username string, password string, connection *gorm.DB) bool {
+
+	type Users struct {
+		Id int
+	    Username string
+		Password string
+	}
+
+	user := Users{Username: username,Password: password}
+    var rows []map[string]any
+    connection.Where("username=? AND password=?",username,password).First(&user).Scan(&rows)
+    return len(rows)==1
+}
+
